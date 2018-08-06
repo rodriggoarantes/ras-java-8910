71ad1645ff47e730fb887506834d482bc957717b
Rodrigo Arantes <rodrigo.arantes@pcinformatica.com.br>
2018-08-06 13:24:12 -0300

    Todo Anotacoes

--- src/main/java/br/com/ras/java/ras_java_8910/App.java
+++ src/main/java/br/com/ras/java/ras_java_8910/App.java
@@ -9,9 +9,24 @@ public class App {
         System.out.println( "Hello World!" );
         System.out.println( "JAVA8!" );
         // DATA
+        // Stream
         System.out.println( "JAVA9!" );
         // JShell
+        // Colleciont.of
+        // List.of Map.of Set.of
+        // Jigsaw
         System.out.println( "JAVA10!" );
         // TODO
+        //  String name = "Rodrigo Turini";
+        // var name = "Rodrigo Turini";
+         
+        // User user = users.findBy(name);
+        // var user = users.findBy(name);
+         
+        // List<String> names = new ArrayList<>();
+        // var names = new ArrayList<String>();
+         
+        // Stream<String> stream = names.stream();
+        // var stream = names.stream();
     }
 }
