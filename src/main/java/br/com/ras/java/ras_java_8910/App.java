2c76ee1c050eac8af732488654618c77e40649ad
Rodrigo Arantes <rodrigo.arantes@pcinformatica.com.br>
2018-08-06 13:05:18 -0300

    Setup JAVA

---
+++ src/main/java/br/com/ras/java/ras_java_8910/App.java
@@ -0,0 +1,17 @@
+package br.com.ras.java.ras_java_8910;
+
+/**
+ * Hello world!
+ */
+public class App {
+  
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.out.println( "JAVA8!" );
+        // DATA
+        System.out.println( "JAVA9!" );
+        // JShell
+        System.out.println( "JAVA10!" );
+        // TODO
+    }
+}
