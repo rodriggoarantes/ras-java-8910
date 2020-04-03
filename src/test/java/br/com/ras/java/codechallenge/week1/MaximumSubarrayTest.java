f4efb40a599e51b65f09d6708d7eac5b9eec2310
rodrigo <rodrigo.arantes@totvs.com.br>
2020-04-03 14:17:23 -0300

    #leetcoding #30DayChallenge Maximum Subarray dia 3 do desafio

---
+++ src/test/java/br/com/ras/java/codechallenge/week1/MaximumSubarrayTest.java
@@ -0,0 +1,64 @@
+package br.com.ras.java.codechallenge.week1;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Test;
+
+public class MaximumSubarrayTest {
+ 
+  private final MaximumSubarray maximumSubarray = new MaximumSubarray();
+  
+  @Test
+  public void test() {
+    assertThat((9/2), equalTo(4));
+  }
+  
+  @Test
+  public void test_mysolution_1() {
+    final int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    assertThat(maximumSubarray.solution(array), equalTo(6));
+  }
+  
+  @Test
+  public void test_mysolution_2() {
+    final int[] array = {4,-1,2,1};
+    assertThat(maximumSubarray.solution(array), equalTo(6));
+  }
+  
+  @Test
+  public void test_mysolution_3() {
+    final int[] array = {-2,1};
+    assertThat(maximumSubarray.solution(array), equalTo(1));
+  }
+  
+  @Test
+  public void test_mysolution_4() {
+    final int[] array = {-2,-1};
+    assertThat(maximumSubarray.solution(array), equalTo(-1));
+  }
+  
+  @Test
+  public void test_alternative_1() {
+    final int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    assertThat(maximumSubarray.alternative(array), equalTo(6));
+  }
+  
+  @Test
+  public void test_alternative_2() {
+    final int[] array = {4,-1,2,1};
+    assertThat(maximumSubarray.alternative(array), equalTo(6));
+  }
+  
+  @Test
+  public void test_alternative_3() {
+    final int[] array = {-2,1};
+    assertThat(maximumSubarray.alternative(array), equalTo(1));
+  }
+  
+  @Test
+  public void test_alternative_4() {
+    final int[] array = {-2,-1};
+    assertThat(maximumSubarray.alternative(array), equalTo(-1));
+  }
+}
