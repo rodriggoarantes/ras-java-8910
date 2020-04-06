f79854f01ecc2d9198f896ba9cb1f0373ad8dadb
rodriggoarantes <rodriggoarantes@gmail.com>
2020-04-06 14:24:44 -0300

    #leetcoding #30DayChallenge Dia6 Group Anagrams 6ms 42,3MB

---
+++ src/test/java/br/com/ras/java/codechallenge/week1/GroupAnagramsTest.java
@@ -0,0 +1,27 @@
+package br.com.ras.java.codechallenge.week1;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GroupAnagramsTest {
+
+    private final GroupAnagrams groupAnagrams = new GroupAnagrams();
+
+    @Test
+    public void test_solution() {
+        final String[] palavras = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        final List<List<String>> resposta = groupAnagrams.solution(palavras);
+        resposta.sort((e1, e2) -> e2.size() - e1.size() );
+
+        assertThat(resposta.size(), equalTo(3));
+        assertThat(resposta.get(0), hasItems("ate","eat","tea"));
+        assertThat(resposta.get(1), hasItems("nat","tan"));
+        assertThat(resposta.get(2), hasItem("bat"));
+    }
+}
