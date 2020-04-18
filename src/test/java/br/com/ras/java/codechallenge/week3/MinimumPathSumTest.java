56af89629a40ba8d0d949f489e3a1d07d0f824d5
rodriggoarantes <rodriggoarantes@gmail.com>
2020-04-18 12:22:24 -0300

    #leetcoding #30DayChallenge Dia18 Minimum Path Sum 3ms

---
+++ src/test/java/br/com/ras/java/codechallenge/week3/MinimumPathSumTest.java
@@ -0,0 +1,18 @@
+package br.com.ras.java.codechallenge.week3;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MinimumPathSumTest {
+
+    private final MinimumPathSum minimumPathSum = new MinimumPathSum();
+
+    @Test
+    public void test_case1() {
+        int[][] grid = new int[][]{{1,3,1},{1,5,1},{4,2,1}};
+        assertThat(minimumPathSum.solution(grid), equalTo(7));
+    }
+
+}
