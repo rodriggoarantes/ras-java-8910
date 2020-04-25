5e59f125bb5b255d41d8fe367bf370a392e5bb2d
rodriggoarantes <rodriggoarantes@gmail.com>
2020-04-25 19:18:12 -0300

    #leetcoding #30DayChallenge Dia25 Jump Game 1ms

--- src/main/java/br/com/ras/java/leetcode/codechallenge/week4/JumpGame.java
+++ src/main/java/br/com/ras/java/leetcode/codechallenge/week4/JumpGame.java
@@ -42,4 +42,14 @@ public class JumpGame {
         }
         return true;
     }
+
+    public boolean alternative(int[] nums) {
+        int lastGood = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastGood) {
+                lastGood = i;
+            }
+        }
+        return lastGood == 0;
+    }
 }
--- src/test/java/br/com/ras/java/leetcode/codechallenge/week4/JumpGameTest.java
+++ src/test/java/br/com/ras/java/leetcode/codechallenge/week4/JumpGameTest.java
@@ -13,29 +13,34 @@ public class JumpGameTest {
     public void test_case1() {
         int[] nums = new int[]{2,3,1,1,4};
         assertThat(jumpGame.canJump(nums), equalTo(true));
+        assertThat(jumpGame.alternative(nums), equalTo(true));
     }
 
     @Test
     public void test_case2() {
         int[] nums = new int[]{3,2,1,0,4};
         assertThat(jumpGame.canJump(nums), equalTo(false));
+        assertThat(jumpGame.alternative(nums), equalTo(false));
     }
 
     @Test
     public void test_case3() {
         int[] nums = new int[]{2,0};
         assertThat(jumpGame.canJump(nums), equalTo(true));
+        assertThat(jumpGame.alternative(nums), equalTo(true));
     }
 
     @Test
     public void test_case4() {
         int[] nums = new int[]{2,0,0};
         assertThat(jumpGame.canJump(nums), equalTo(true));
+        assertThat(jumpGame.alternative(nums), equalTo(true));
     }
 
     @Test
     public void test_case5() {
         int[] nums = new int[]{4,0,4,2,2,0,1,3,3,0,3};
         assertThat(jumpGame.canJump(nums), equalTo(true));
+        assertThat(jumpGame.alternative(nums), equalTo(true));
     }
 }
