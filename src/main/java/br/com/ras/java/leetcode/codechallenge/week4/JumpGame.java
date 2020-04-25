5c93c43af27f2dd5642af7502331c0e413271e52
rodriggoarantes <rodriggoarantes@gmail.com>
2020-04-25 19:11:40 -0300

    #leetcoding #30DayChallenge Dia25 Jump Game 1ms

---
+++ src/main/java/br/com/ras/java/leetcode/codechallenge/week4/JumpGame.java
@@ -0,0 +1,45 @@
+package br.com.ras.java.leetcode.codechallenge.week4;
+
+/**
+ * Jump Game
+ *
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ *
+ * Each element in the array represents your maximum jump length at that position.
+ *
+ * Determine if you are able to reach the last index.
+ *
+ * Example 1:
+ *
+ * Input: [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ *
+ * Example 2:
+ *
+ * Input: [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what. Its maximum
+ *              jump length is 0, which makes it impossible to reach the last index.
+ */
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        if (nums.length == 1) {
+            return true;
+        }
+
+        int jumper = 0;
+        int size = nums.length;
+
+        for (int i=0; i < size; i++) {
+            if (jumper < i) {
+                return false;
+            }
+            jumper = Math.max(jumper, i + nums[i]);
+        }
+        return true;
+    }
+}
